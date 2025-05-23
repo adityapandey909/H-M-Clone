cb587061f20cb61359bb39c1852b8a2913073bb1
adityapandey909 <pandeyaditya44517@gmail.com>
2025-05-24 05:14:42 +0530

    first.commit

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-// Add a click event to the Cart icon
 const cartIcon = document.querySelector(".fa-shopping-bag");
-
-cartIcon.addEventListener("click", function() {
-    alert("🛒 Your cart is empty.");
-});
-
-// Add a click event to the User icon
+const cartPanel = document.querySelector(".cart-panel");
 const userIcon = document.querySelector(".fa-user");
 
+// Add a click event to the User icon
 userIcon.addEventListener("click", function() {
     alert("👤 Please sign into your account.");
 });
 
 // Toggle the Cart Panel
-
-// Toggle the Cart Panel
-const cartPanel = document.querySelector(".cart-panel");
-
-cartPanel.addEventListener("click", function() {
+cartIcon.addEventListener("click", function() {
     cartPanel.classList.toggle("show");
     cartPanel.classList.toggle("hidden");
-})
+});
