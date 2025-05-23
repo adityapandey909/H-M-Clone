0e82db0a722aa6e3abef8e4463bd782b80ec8f61
adityapandey909 <pandeyaditya44517@gmail.com>
2025-05-24 04:22:31 +0530

    first.commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add a click event to the Cart icon
+const cartIcon = document.querySelector(".fa-shopping-bag");
+
+cartIcon.addEventListener("click", function() {
+    alert("🛒 Your cart is empty.");
+});
+
+// Add a click event to the User icon
+const userIcon = document.querySelector(".fa-user");
+
+userIcon.addEventListener("click", function() {
+    alert("👤 Please sign into your account.");
+});
+
+// Toggle the Cart Panel
+
